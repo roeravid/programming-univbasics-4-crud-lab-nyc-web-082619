31e0b489ffe079f347407d0f82d39bb85825e33a
Joe David <javid92@gmail.com>
2019-08-16 11:33:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,15 +7,15 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array << "4"
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  array.shift("4")
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.
 end
 
 def remove_element_from_start_of_array(array)
